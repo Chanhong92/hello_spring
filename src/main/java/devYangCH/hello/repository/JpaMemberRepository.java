9b3b8e850b300bcb4edf2895ac4007ef521fb8e4
jeyang <ych1273@gmail.com>
2022-03-05 05:20:31 +0900

    스프링 DB:JPA

---
+++ src/main/java/devYangCH/hello/repository/JpaMemberRepository.java
@@ -0,0 +1,47 @@
+package devYangCH.hello.repository;
+
+import devYangCH.hello.domain.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository {
+
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+        return result.stream().findAny();
+
+        // Ctrl + Alt + N 누르면 아래 처럼 인라인으로 바뀐다
+//        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+//                .setParameter("name", name)
+//                .getResultList().stream().findAny();
+    }
+}
--- src/main/java/devYangCH/hello/service/MemberService.java
+++ src/main/java/devYangCH/hello/service/MemberService.java
@@ -6,9 +6,11 @@ import devYangCH.hello.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional //회원가입 쪽에만 해도 되긴 함
 public class MemberService {
 
     private final MemberRepository memberRepository;
